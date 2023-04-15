03c2519302772f147ffc4925d88eabaddbc43e66
Sergey Koshman <unex1488@gmail.com>
2023-04-15 20:16:40 +0500

    commit

---
+++ ball/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class BallConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ball'
+    verbose_name = 'Футболисты Мира'
